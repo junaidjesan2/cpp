4120c074d307682c8a02f6bdbe036d62b58d0205
junaidjesan2 <junaidjesan2@gmail.com>
2025-05-27 18:38:07 +0600

    dynamic array learning

---
+++ week-01/basic/dynamic_array.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int* dy_ar(){
+    int* x=new int[5];
+    for(int i=0;i<5;i++){
+        cin>>x[i];
+    }
+    return x;
+}
+int main()
+{
+    int* a=dy_ar();
+    for(int i=0;i<5;i++){
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
---
+++ week-01/basic/eof.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "Enter a string (end with Ctrl+D or Ctrl+Z):" << endl;
+    return 0;
+}
